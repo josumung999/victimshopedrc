136e5a42963553aff1134bdc218238f566831fff
Josué Munganga <jmunganga039@gmail.com>
2022-01-19 17:00:24 +0200

    Still working with the dark theme

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 const Hero = () => {
     return (
-      <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
+      <div className="relative dark:bg-gray-900 flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
         <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
           <svg
-            className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
+            className="absolute left-0 hidden h-full text-white dark:text-gray-900 transform -translate-x-1/2 lg:block"
             viewBox="0 0 100 100"
             fill="currentColor"
             preserveAspectRatio="none slice"
