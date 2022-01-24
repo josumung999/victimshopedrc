45fc4cf886f576dcd92acc592d33040e84334036
Josué Munganga <jmunganga039@gmail.com>
2022-01-24 11:01:04 +0200

    Worked on misson page

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Link from "next/link"
 
 const { faBullseye } = require("@fortawesome/free-solid-svg-icons")
 const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome")
@@ -28,20 +28,22 @@ const Mission = () => {
             </p>
           </div>
           <div>
-            <a
-              href="/"
+            <Link
+              href="/domaines"
               aria-label=""
-              className="inline-flex items-center font-semibold font-Raleway transition-colors duration-200 text-purple-400 hover:text-purple-800"
             >
-              En Savoir Plus
-              <svg
-                className="inline-block w-3 ml-2"
-                fill="currentColor"
-                viewBox="0 0 12 12"
-              >
-                <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
-              </svg>
-            </a>
+              <a className="inline-flex items-center font-semibold font-Raleway transition-colors duration-200 text-purple-400 hover:text-purple-800">
+                Domaines d'Intervention
+                <svg
+                  className="inline-block w-3 ml-2"
+                  fill="currentColor"
+                  viewBox="0 0 12 12"
+                >
+                  <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+                </svg>
+              </a>
+              
+            </Link>
           </div>
         </div>
         <div className="flex items-center justify-center -mx-4 lg:pl-8">
