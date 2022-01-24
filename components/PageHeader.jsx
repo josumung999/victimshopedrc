f23a554911a9071ac89d69160e928ca17109a99c
Josué Munganga <jmunganga039@gmail.com>
2022-01-24 11:31:26 +0200

    Added bg image to pageheader component

--- components/PageHeader.jsx
+++ components/PageHeader.jsx
@@ -1,12 +1,12 @@
 const PageHeader = (props) => {
   return (
-    <div className="bg-gray-700">
-      <div className="px-4 py-16 dark:bg-gray-900 flex flex-row justify-center">
+    <div className="relative bg-fixed bg-center bg-cover bg-no-repeat" style={{ backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(/images/header.jpg)" }}>
+      <div className="px-4 py-16 flex flex-row justify-center">
         <div className="basis-1/2">
-          <h1 className="font-Raleway text-gray-200 dark:text-gray-200 font-black text-3xl sm:text-4xl">
+          <h1 className="font-Raleway text-gray-100 font-black text-3xl sm:text-4xl">
             {props.title}
           </h1>
-          <p className="font-Roboto font-semibold text-xl text-purple-400 dark:text-purple-300">
+          <p className="font-Roboto font-semibold text-xl text-slate-200">
             {props.description}
           </p>
         </div>
