621dca94682934df81f7266fa3bf3fcf25e86d20
Josué Munganga <jmunganga039@gmail.com>
2022-01-24 10:53:35 +0200

    Optimizing the mission section

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 const { faBullseye } = require("@fortawesome/free-solid-svg-icons")
 const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome")
 
@@ -45,21 +47,21 @@ const Mission = () => {
         <div className="flex items-center justify-center -mx-4 lg:pl-8">
           <div className="flex flex-col items-end px-3">
             <img
-              className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-              src="https://images.pexels.com/photos/3207532/pexels-photo-3207532.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              src="/images/img-mission-1.jpeg"
               alt=""
+              className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
             />
             <img
-              className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-              src="https://reliefweb.int/sites/reliefweb.int/files/styles/report-large/public/report-images/rdc_till-mayer-kongo52.jpg"
+              src="/images/img-mission-2.jpg"
               alt=""
+              className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
             />
+            
           </div>
           <div className="px-3">
             <img
               className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-              src="https://www.worldvision.org/wp-content/uploads/2021/05/W324-0052-003-831x566.jpg"
-              alt=""
+              src="/images/img-mission-3.jpg"
             />
           </div>
         </div>
