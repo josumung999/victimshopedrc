e61d13768a3accaa602988c735a75d00cad437ba
Josué Munganga <jmunganga039@gmail.com>
2022-02-01 13:59:25 +0200

    Started working on the mail API

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,51 +1,91 @@
-const Contact = () => {
+import { faFacebook, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"
+
+const { faPhoneAlt, faEnvelope, faMapMarkerAlt } = require("@fortawesome/free-solid-svg-icons")
+const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome")
+
+const Contact = (props) => {
   return (
     <div className="">
-      <div className="flex flex-col sm:flex-row">
-          <div className="basis-1/3 bg-slate-700">
-            01
+      <div className="flex flex-col-reverse md:flex-row">
+          <div className="py-4 px-4 sm:py-10 sm:px-10 basis-1/3 bg-slate-700 dark:bg-slate-900">
+            <div>
+              <h2 className="mt-4 mb-10 font-Raleway text-2xl font-bold leading-none tracking-tight text-gray-100 dark:text-gray-300 sm:text-3xl md:mx-auto">
+                Coordonnées
+              </h2>
+            </div>
+            <div className="flex flex-col">
+              <div className="inline-flex items-center mb-4">
+                <FontAwesomeIcon icon={faPhoneAlt} className="text-gray-100 dark:text-gray-300 mr-4 w-6 h-6" />
+                <span className="text-base text-gray-100 dark:text-gray-300">
+                  +243 991 234 567
+                </span>
+              </div>
+              <div className="inline-flex items-center mb-4">
+                <FontAwesomeIcon icon={faEnvelope} className="text-gray-100 dark:text-gray-300 mr-4 w-6 h-6" />
+                <span className="text-base text-gray-100 dark:text-gray-300">
+                  contact@victimshopedrc.org
+                </span>
+              </div>
+              <div className="inline-flex items-center mb-4">
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="text-gray-100 dark:text-gray-300 mr-4 w-6 h-6" />
+                <span className="text-base text-gray-100 dark:text-gray-300">
+                  N°342 Avenue PE Lumumba, Bukavu, RD Congo
+                </span>
+              </div>
+              <div className="py-6 px-16 inline-flex justify-between">
+                <a href="!#" className="">
+                  <FontAwesomeIcon icon={faFacebook} className="text-gray-100 dark:text-gray-300 hover:text-purple-700 dark:hover:text-purple-400 mr-4 w-10 h-10" />
+                </a>
+                <a href="!#" className="">
+                  <FontAwesomeIcon icon={faLinkedin} className="text-gray-100 dark:text-gray-300 hover:text-purple-700 dark:hover:text-purple-400 mr-4 w-10 h-10" />
+                </a>
+                <a href="!#" className="">
+                  <FontAwesomeIcon icon={faTwitter} className="text-gray-100 dark:text-gray-300 hover:text-purple-700 dark:hover:text-purple-400 w-10 h-10" />
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="py-10 px-10 basis-2/3 bg-slate-200 flex flex-col justify-center">
+          <div className="py-4 px-4 sm:py-10 sm:px-10 basis-2/3 bg-slate-200 dark:bg-slate-700 flex flex-col items-center justify-center">
           <div>
             <h2 className="mt-4 mb-10 font-Raleway text-2xl font-bold leading-none tracking-tight text-gray-900 dark:text-gray-300 sm:text-3xl md:mx-auto">
               Laissez-nous un Message
             </h2>
           </div>
-            <form className="w-full max-w-lg flex justify-center flex-col font-Roboto">
+            <form method="post" onSubmit={props.handleOnSubmit} className="w-full max-w-lg flex justify-center flex-col font-Roboto">
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
-                  <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                  <label className="block tracking-wide text-gray-700 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-first-name">
                     Nom Complet
                   </label>
-                  <input className="block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="John Doe" />
+                  <input className="block w-full bg-gray-200 dark:bg-gray-600 text-gray-700 dark:text-gray-400 border dark:border-none border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200" name="name" type="text" placeholder="John Doe" />
                 </div>
                 <div className="w-full md:w-1/2 px-3">
-                  <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                  <label className="block tracking-wide text-gray-700 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-phone">
                     Téléphone
                   </label>
-                  <input className="block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="phone" placeholder="+243 991 234 567" />
+                  <input className="block w-full bg-gray-200 dark:bg-gray-600 text-gray-700 dark:text-gray-400 border dark:border-none border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200" name="phone" type="phone" placeholder="+243 991 234 567" />
                 </div>
               </div>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                  <label className="block tracking-wide text-gray-700 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-password">
                     E-mail
                   </label>
-                  <input className="block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" />
+                  <input className="block w-full bg-gray-200 dark:bg-gray-600 text-gray-700 dark:text-gray-400 border dark:border-none border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200" name="email" type="email" />
                 </div>
               </div>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                  <label className="block tracking-wide text-gray-700 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-password">
                     Message
                   </label>
-                  <textarea className=" no-resize block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
-                  <p className="text-gray-600 text-xs italic">Laissez-nous un message</p>
+                  <textarea className="no-resize block w-full bg-gray-200 dark:bg-gray-600 text-gray-700 dark:text-gray-400 border dark:border-none border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200 h-48 resize-none" name="message"></textarea>
+                  <p className="text-gray-600 dark:text-gray-300 text-xs italic">Laissez-nous un message</p>
                 </div>
               </div>
               <div className="md:flex md:items-center mt-6">
                 <div className="md:w-1/3">
-                  <button className="shadow bg-purple-400 hover:bg-purple-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                  <button className="block w-full shadow bg-purple-400 hover:bg-purple-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
                     Envoyer
                   </button>
                 </div>
--- pages/contact.js
+++ pages/contact.js
@@ -3,6 +3,16 @@ import ContainerBlock from "../components/ContainerBlock"
 import PageHeader from "../components/PageHeader"
 
 const contact = () => {
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    const formData = {}
+    Array.from(e.currentTarget.elements).forEach(field => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    })
+    console.log(formData);
+  }
+
   return (
     <ContainerBlock
       title="Nous Contacter - Victim's Hope DRC"
@@ -13,7 +23,7 @@ const contact = () => {
         description="Rejoignez notre communauté aujourd'hui, laissez nous un message et nous serions ravis de vous répondre"
         bg="/images/bg-contact.jpeg"
       />
-      <Contact />
+      <Contact handleOnSubmit={handleOnSubmit} />
     </ContainerBlock>
   )
 }
