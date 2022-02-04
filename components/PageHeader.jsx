f248e3583a6dadbc6cb4200a9a120f0bd2bf0199
Josué Munganga <jmunganga039@gmail.com>
2022-02-04 15:36:41 +0200

    Started working on the the donation page

--- components/PageHeader.jsx
+++ components/PageHeader.jsx
@@ -1,9 +1,9 @@
 const PageHeader = (props) => {
   return (
     <div className="relative bg-fixed bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${props.bg})` }}>
-      <div className="px-4 py-16 flex flex-row justify-center">
-        <div className="basis-1/2">
-          <h1 className="font-Raleway text-gray-100 font-black text-3xl sm:text-4xl">
+      <div className="px-6 py-16 flex flex-row justify-center">
+        <div className="sm:basis-1/2">
+          <h1 className="mb-4 font-Raleway text-gray-100 font-black text-3xl sm:text-4xl">
             {props.title}
           </h1>
           <p className="font-Roboto font-semibold text-xl text-slate-200">
--- pages/donate.js
+++ pages/donate.js
@@ -1,4 +1,5 @@
 import ContainerBlock from "../components/ContainerBlock"
+import PageHeader from "../components/PageHeader"
 
 const donate = () => {
   return (
@@ -6,7 +7,11 @@ const donate = () => {
       title="Faire un Don - Victim's Hope DRC"
       description="Faire un Don à Victim's Hope pour soutenir nos projets humanitaire"
     >
-      Faire un Don
+      <PageHeader
+        title="Faire un Don"
+        description="Étant une organisation à but non lucratif, nous apportons notre soutiens à victimes grâce au soutien de la communauté internationale, Rejoignez notre cause en soutenant nos différents projets"
+        bg='/images/bg-donation.jpg'
+      />
     </ContainerBlock>
   )
 }
